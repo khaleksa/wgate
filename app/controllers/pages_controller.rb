ce75fafb9b9dd2dd3c50b52e5924180fbc10b0cc
Alexandra Khegay <khaleksa@gmail.com>
2015-06-30 16:20:05 +0500

    add sorting for payments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,11 @@ class PagesController < ApplicationController
   force_ssl if: :ssl_configured?
 
   def index
-    @payments = Payment.where(provider_id: 3, status: 'commited')
+    @payments = Payment.where(provider_id: 3, status: 'commited').order(:created_at)
     @total_amount = @payments.pluck(:amount).sum
   end
 
   def access_errors
-    @account_errors = AccessError.where(provider_id: 3)
+    @account_errors = AccessError.where(provider_id: 3).order(:created_at)
   end
 end
